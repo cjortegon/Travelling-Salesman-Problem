a1a4afe56111392f9af03be99782b8e6e6dc0760
cjortegon <cjortegon@gmail.com>
2015-10-29 20:11:09 -0500

    Initial commit

---
+++ Travelling-Salesman-Problem/src/graph/Graph.java
@@ -0,0 +1,15 @@
+package graph;
+
+import java.util.ArrayList;
+
+public class Graph {
+
+	public ArrayList<Node> nodes;
+	public ArrayList<Edge> edges;
+
+	public Graph() {
+		this.nodes = new ArrayList<>();
+		this.edges = new ArrayList<>();
+	}
+
+}
